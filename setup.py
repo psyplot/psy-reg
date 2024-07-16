9565c4416ce3db59bb324a0c96ec168194a1a65c
Philipp Sommer <philipp.sommer@hereon.de>
2024-07-16 15:33:55 +0200

    re-add setup.py file
    has accidently been removed in !11

---
+++ setup.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2021-2024 Helmholtz-Zentrum hereon GmbH
+#
+# SPDX-License-Identifier: CC0-1.0
+
+"""Setup script for the psy-reg package."""
+import versioneer
+from setuptools import setup
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
